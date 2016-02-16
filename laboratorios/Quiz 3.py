fd8f80ad65a213119a8535ea8822d12712346fba
lwong319 <wongluis319@uip.edu.pa>
2016-02-15 21:38:03 -0500

    remover errores
    corregido

--- laboratorios/Quiz 3.py
+++ laboratorios/Quiz 3.py
@@ -2,13 +2,15 @@
 lista_super=[]
 lista =["arroz","leche","tuna","cereal","jugo"]
 
-print("Listado de supermercado")
-print("1.Agragar Articulo")
-print("2.Eliminar Atriculo")
-print("3.Ver Lista")
-print("4.salir")
-lis = int(input())
-while lis!=4:
+lis = 2
+while  lis!=0 :
+    print("Listado de supermercado")
+    print("1.Agragar Articulo")
+    print("2.Eliminar Atriculo")
+    print("3.Ver Lista")
+    print("4.salir")
+    lis = int(input())
+
     if lis == 1:
         print(lista)
         print("Que articulo desea agragar: ")
@@ -16,41 +18,26 @@ while lis!=4:
         lista_super.append(a)
         print(lista)
         print(lista_super)
-        print("1.Agragar Articulo")
-        print("2.Eliminar Atriculo")
-        print("3.Ver Lista")
-        print("4.salir")
-        lis = int(input())
     elif lis == 2:
         print(lista_super)
         print("Que articulo desea elimino: ")
         b=input()
         try:
-            lista_super.remove(str(b))
-            lis = int(input())
-            print("1.Agragar Articulo")
-            print("2.Eliminar Atriculo")
-            print("3.Ver Lista")
-            print("4.salir")
-            lis = int(input())
+            #lista_super.remove(int(b))
+            lista_super.pop(int(b))
+
         except ValueError:
             print("producto no en lisa")
             print(lista)
             print(lista_super)
-            print("1.Agragar Articulo")
-            print("2.Eliminar Atriculo")
-            print("3.Ver Lista")
-            print("4.salir")
-            lis = int(input())
+
+
     elif lis == 3:
         print("El listado de articulos: ")
         print(lista)
         print(lista_super)
-        print("1.Agragar Articulo")
-        print("2.Eliminar Atriculo")
-        print("3.Ver Lista")
-        print("4.salir")
-        lis = int(input())
+
+
     else:
         lis ==4
         print(lista)
